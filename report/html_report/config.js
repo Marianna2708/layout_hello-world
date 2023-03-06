e18450c21a37bf62dd91c00946ee73523ad05a46
Marianna Hawrysh <hawrysh.marianna@gmail.com>
2023-03-06 22:06:34 +0200

    make build

--- report/html_report/config.js
+++ report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Entire_document.png",
-        "test": "..\\bitmaps_test\\20230304-194002\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20230306-214906\\Entire_document.png",
         "selector": "document",
         "fileName": "Entire_document.png",
         "label": "Entire document",
